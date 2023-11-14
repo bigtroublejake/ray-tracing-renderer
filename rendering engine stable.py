1a080050e82041d371c42d5d52e149b81c72c866
bigtroublejake <69482919+bigtroublejake@users.noreply.github.com>
2023-11-13 23:41:48 -0500

    Soft shadow optimized
    Helps performance a lot

--- rendering engine stable.py
+++ rendering engine stable.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+import time
+
+start_time = time.perf_counter()
 
 def normalize(vector):
     return vector / np.linalg.norm(vector)
@@ -28,13 +32,36 @@ def nearest_intersected_object(objects, ray_origin, ray_direction):
             nearest_object = objects[index]
     return nearest_object, min_distance
 
+def shadow_check(objects, origin, direction, light_center):
+    nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
+    if nearest_object is None:
+        return None
+
+    # compute intersection point between ray and nearest object
+    intersection = origin + min_distance * direction
+
+    normal_to_surface = normalize(intersection - nearest_object['center'])
+    shifted_point = intersection + 1e-5 * normal_to_surface
+    intersection_to_light = normalize(light_center - shifted_point)
+
+    _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
+    intersection_to_light_distance = np.linalg.norm(light_center - intersection)
+    is_shadowed = min_distance < intersection_to_light_distance
+
+    if is_shadowed:
+        return True
+    else:
+        return False
+
 
 
 width = 300
 height = 200
 
-max_depth = 2
+max_depth = 1
 soft_shadows = True
+light_range = 3
+shadow_test_values = [1,-1,0]
 
 camera = np.array([0, 0, 1])
 ratio = float(width) / height
@@ -42,7 +69,7 @@ screen = (-1, 1 / ratio, 1, -1 / ratio) # left, top, right, bottom
 sky_color = np.array([0.35, 0.65, 1])
 
 
-light = { 'center': np.array([5, 5, 5]), 'radius': 2, 'n_checks': 1, 'ambient': np.array([1, 1, 1]), 'diffuse': np.array([1, 1, 1]), 'specular': np.array([1, 1, 1]) }
+light = { 'center': np.array([5,5,5]), 'radius': 2/3, 'n_checks': 1, 'ambient': np.array([1, 1, 1]), 'diffuse': np.array([1, 1, 1]), 'specular': np.array([1, 1, 1]) }
 
 objects = [
     { 'center': np.array([-0.5, 0, -1]), 'radius': 0.7, 'ambient': np.array([0.1, 0, 0]), 'diffuse': np.array([0.7, 0, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.4 },
@@ -53,6 +80,8 @@ objects = [
 #    { 'center': np.array([0, 0, -50000]), 'radius': 49900, 'ambient': np.array([0.35, 0.65, 1]), 'diffuse': np.array([0, 0, 0]), 'specular': np.array([0, 0, 0]), 'shininess': 0, 'reflection': 0.1 }
 
 
+total_s = 0
+total_ns = 0
 
 image = np.zeros((height, width, 3))
 for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
@@ -65,32 +94,43 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
         color = np.zeros((3))
         reflection = 1
         n_shadowed = 0
+        test_light_flag = 0
 
         if soft_shadows:
-            for l in range(-2, 2 +1):
-                for m in range(-2, 2 +1):
 
-                    light_center = [light['center'][0] + l/5, light['center'][1] + m/5, light['center'][2]]
+            for a in list(itertools.product(shadow_test_values, shadow_test_values)):
 
-                    nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
-                    if nearest_object is None:
-                        break
+                test_lighting_pos = [light['center'][0] + light['radius'] * a[0], light['center'][1] + light['radius'] * a[1], light['center'][2]]
 
-                    # compute intersection point between ray and nearest object
-                    intersection = origin + min_distance * direction
+                shadow_test_flag = shadow_check(objects, origin, direction, test_lighting_pos)
 
-                    normal_to_surface = normalize(intersection - nearest_object['center'])
-                    shifted_point = intersection + 1e-5 * normal_to_surface
-                    intersection_to_light = normalize(light_center - shifted_point)
+                if shadow_test_flag is None:
+                    break
+                elif shadow_test_flag is True:
+                    test_light_flag += 1
+                elif shadow_test_flag is False:
+                    test_light_flag -= 1
 
-                    _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
-                    intersection_to_light_distance = np.linalg.norm(light_center - intersection)
-                    is_shadowed = min_distance < intersection_to_light_distance
 
-                    if is_shadowed:
-                        n_shadowed += 1 
+            if test_light_flag == len(list(itertools.product(shadow_test_values, shadow_test_values))):
+                reflection = 0 
+                total_s += 1
+            elif test_light_flag == -len(list(itertools.product(shadow_test_values, shadow_test_values))):
+                reflection = 1
+                total_ns +=1
+            else:
+                for l in range(-light_range , light_range +1):
+                    for m in range(-light_range , light_range +1):
+
+                        light_center = [light['center'][0] + l*light['radius']/light_range, light['center'][1] + m*light['radius']/light_range, light['center'][2]]
+
+                        shadow_check_flag = shadow_check(objects, origin, direction, light_center)
+
+                        if shadow_check_flag is True:
+                            reflection -= 1 / (light_range*2+1)**2
+                        if shadow_check_flag is None:
+                            break
         
-        reflection = 1 - n_shadowed/25
 
         for k in range(max_depth):
             
@@ -102,6 +142,7 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
                     color = sky_color
                 break
 
+            
             
             # compute intersection point between ray and nearest object
             intersection = origin + min_distance * direction
@@ -153,3 +194,5 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
 
 
 plt.imsave('image.png', image)
+print(total_s, total_ns)
+print(time.perf_counter() - start_time)
